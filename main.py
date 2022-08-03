62581f3f116ca788dd5e085d4c891dca99e042db
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-08-03 03:05:02 +0300

    add comments

--- main.py
+++ main.py
@@ -6,11 +6,16 @@
 def num_of_easy_number(n):
     list_of_easy_numbers = [2]
     while len(list_of_easy_numbers) < n:
-        for j in range(1, 99999999999999999):
-            for i in list_of_easy_numbers:
+        for j in range(2, 99999999999999999):
+            for i in list_of_easy_numbers:      #добавить счетчик чтобы считал что простое число не делиться на другие простые числа
+                                                # и если не делится на остальные простые, то только тогда его можно записывать
+                                                # логично же? похуй, 3 ночи, завтра додумаю
                 if j % i != 0:
                     print(j % i)
-                    if list_of_easy_numbers.count(j % i) == 0:  # число должно добавляться только если оно не делится на каждое из чисел из списка
+                    if list_of_easy_numbers.count(j) == 0:  # число должно добавляться только если оно не делится на каждое из чисел из списка
+                        for k in list_of_easy_numbers:
+                            j % list_of_easy_numbers[k]
+
                         list_of_easy_numbers.append(j)
                         break
                 if len(list_of_easy_numbers) == n:
